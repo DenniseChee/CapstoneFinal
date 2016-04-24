c01aaf432b20e04934bd876f13bae2cb4eded64b
Dennise Chee <dennischee23@gmail.com>
2016-04-24 16:10:36 +0800

    finalProject

---
+++ ui.R
@@ -0,0 +1,34 @@
+
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(fluidPage(
+  
+  # Application title.
+  titlePanel("Word Prediction"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("obs", "Please Enter Your Statement Here:"),
+      
+      helpText("Note: We will use this statement to predict the most likely next word."),
+      
+      submitButton("Submit")
+    ),
+    
+    mainPanel(
+      h6("You input the following text:"),
+      textOutput("Original"),
+      br(),
+      #h6("Your statement has been reformated to the following:"),
+      #textOutput("Translated"),
+      #br(),
+      br(),
+      h3("Most Likely Next Word:"),
+      div(textOutput("BestGuess"), style = "color:red"),
+      br(),
+      h3("The program guessed your word based on the following data:"),
+      tableOutput("view")
+    )
+  )
+))
